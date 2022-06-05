0c1407a70b644523e8258f53b66d475bfec4a896
Ben Pettis <bpettis@wisc.edu>
2022-06-05 13:52:36 -0500

    display basic cosine and euclidean similarities

--- find-newspapers.py
+++ find-newspapers.py
@@ -6,6 +6,10 @@
 # The list should be a .txt file and contain one IA identifier on each line
 
 import internetarchive, json, requests
+import glob
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from scipy.spatial.distance import pdist, squareform
 
 listName = 'pressbook-search-list.txt' # Specify the file that contains IA identifiers
 yearRange = 5 # Specify how many years after the Pressbook date we should search within
@@ -37,8 +41,45 @@ def findNewspapers(pressbooks):
 				filename = outputDirectory + '/' + i.item_metadata['metadata']['identifier'] + '/' + cleanID + '.txt'
 				with open(filename, 'w') as f:
 					f.write(page['ocr_eng'])
+			print('Downloaded some files to work with!')
+			textSimilarity(i.item_metadata['metadata']['identifier'])
 			
 
+def textSimilarity(identifier):
+	# Make a list of file names
+	filenames = glob.glob(outputDirectory + '/' + identifier + '/*.txt')
+	# Parse the list of file names to get a list of keys (ID numbers) for us to use later
+	filekeys = [f.split('/')[-1].split('.')[0] for f in filenames]
+	
+	# Create a CountVectorizer instance with the parameters you need
+	vectorizer = CountVectorizer(input="filename", max_features=1000, max_df=0.7)
+	# Run the vectorizer on your list of filenames to create your wordcounts
+	# Use the toarray() function so that SciPy will accept the results
+	wordcounts = vectorizer.fit_transform(filenames).toarray()
+	comparison_target = identifier + '_djvu' # SciPy expects the filekey to match the exact file name, which includes this little bit of extra
+	
+	# Euclidean Distances
+	print("\n\nEUCLIDEAN DISTANCES\n*******************")
+	euclidean_distances = pd.DataFrame(squareform(pdist(wordcounts)), index=filekeys, columns=filekeys)
+	print(euclidean_distances)
+	print('\n\n')
+	print('TOP 5 TEXTS THAT ARE MOST SIMILAR TO ' + comparison_target + ":")
+	top5_euclidean = euclidean_distances.nsmallest(6, comparison_target)[comparison_target][1:]
+	print(top5_euclidean)
+	print('(Lower score is more similar)')
+	
+	
+	# Cosine Distances
+	print("\n\nCOSINE DISTANCES\n***************")
+	cosine_distances = pd.DataFrame(squareform(pdist(wordcounts, metric='cosine')), index=filekeys, columns=filekeys)
+	print(cosine_distances)
+
+	print('TOP 5 TEXTS THAT ARE MOST SIMILAR TO ' + comparison_target + ":")
+	top5_cosine = cosine_distances.nsmallest(6, comparison_target)[comparison_target][1:]
+	print(top5_cosine)
+	print('(Ranges from 0 - 1. Lower score is more similar)')
+	print('\n\n')
+	
 
 def formQuery(yearOne, yearTwo):
 	query = 'https://chroniclingamerica.loc.gov/search/pages/results?' + 'year1=' + str(yearOne) + '&year2=' + str(yearTwo) + '&format=json' + '&language=eng'
